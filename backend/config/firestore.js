24d5e69d3dfaa34f69b233aa3d3390f0c03fb2fe
MRERROR2050 <abdalfatah.aljuaidi@gmail.com>
2026-04-25 12:40:41 +0300

    d

--- backend/config/firestore.js
+++ backend/config/firestore.js
@@ -1,11 +1,10 @@
 const admin = require('firebase-admin');
-const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
 
 admin.initializeApp({
  credential: admin.credential.cert({
    projectId: process.env.FIREBASE_PROJECT_ID,
    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-   privateKey
+   privateKey:process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
  })
 });
 
